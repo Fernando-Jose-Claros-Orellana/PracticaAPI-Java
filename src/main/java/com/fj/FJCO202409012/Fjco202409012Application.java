0f69053fbce47e23637ae6e5e294b03cae0fb086
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-12 09:22:40 -0600

    Se inicializa

---
+++ src/main/java/com/fj/FJCO202409012/Fjco202409012Application.java
@@ -0,0 +1,13 @@
+package com.fj.FJCO202409012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fjco202409012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fjco202409012Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fj/FJCO202409012/Fjco202409012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fj.FJCO202409012;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fjco202409012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
